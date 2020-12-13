6d8ec3c6dad5b32de8b29cf8abc8f38dee6eb399
shanecbauman <shanebauman11@gmail.com>
2020-12-12 19:41:28 -0800

    Day 10 finished

--- 10/secondhalf.py
+++ 10/secondhalf.py
@@ -6,7 +6,7 @@ os.chdir('./10')
 # examplepuzzle_2.txt
 # puzzleinput.txt
 
-with open('examplepuzzle.txt', 'r') as readfile:
+with open('puzzleinput.txt', 'r') as readfile:
     puzzleinput = readfile.read().split('\n')
 nums = [[0]]
 
@@ -20,10 +20,26 @@ jolt_1 = 0
 jolt_2 = 0
 jolt_3 = 0
 joltage = 0
+variations = 0
+
+def lookback(last_3, joltage):
+    count = 0
+    for num in last_3:
+        if num[0] >= joltage - 3:
+            count += num[1]
+    return count
 
 for i, num in enumerate(nums):
-    diff = num - joltage
-    joltage = num
+    if i == 0:
+        num.append(1)
+    else:
+        lbk_count = i - 3
+        if lbk_count < 0:
+            lbk_count = 0
+        num.append(lookback(nums[lbk_count:i], num[0]))
+    variations += num[1]
+    diff = num[0] - joltage
+    joltage = num[0]
     if diff == 1:
         jolt_1 += 1
     elif diff == 2:
@@ -33,4 +49,5 @@ for i, num in enumerate(nums):
     if i == len(nums) - 1:
         jolt_3 += 1
 
-print(jolt_1 * jolt_3)
+# print(jolt_1 * jolt_3)
+print(nums[-1][1])
